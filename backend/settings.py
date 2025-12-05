a7e6df04c89d33275edcf56a87216fb2d781da7d
arpitpatel2406 <arpitpatel2406@gmail.com>
2025-12-05 18:24:59 +0530

    add_import

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
